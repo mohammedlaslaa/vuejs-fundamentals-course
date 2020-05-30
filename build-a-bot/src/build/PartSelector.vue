7b8ea6325d6b55208c25a77ded300a8dd7a60a8c
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-05-30 14:27:24 +0200

    seventh section

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -9,11 +9,14 @@
     /></router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{ bottom: '10px', right: '50px' }" class="sale" v-show="selectedPart.onSale"
+      >Sale!</span
+    >
   </div>
 </template>
 
 <script>
+
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -71,9 +74,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
---
+++ build-a-bot/src/shared/pin-directive.js
@@ -0,0 +1,8 @@
+/* eslint no-param-reassign: ["error", { "ignorePropertyModificationsFor": ["element"] }] */
+
+export default function (element, binding) {
+  Object.keys(binding.value).forEach((key) => {
+    element.style[key] = binding.value[key];
+  });
+  element.style.position = 'absolute';
+}
