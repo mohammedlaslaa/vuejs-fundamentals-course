e33d5d1f52899c6ee14caef5c1a817c0589d182f
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-05-30 15:05:44 +0200

    finished course

--- build-a-bot/vue.config.js
+++ build-a-bot/vue.config.js
@@ -1,4 +1,30 @@
 module.exports = {
+  configureWebpack: config => {
+    config.module.rules.push({ test: /\.coffee$/, use: ['coffee-loader'] });
+    const newRule = {
+      test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+      use: [
+        {
+          loader:
+            'C:\\Users\\moham\\Desktop\\vuejs-fundamentals-course\\build-a-bot\\node_modules\\url-loader\\dist\\cjs.js',
+          options: {
+            limit: 8096,
+            fallback: {
+              loader:
+                'C:\\Users\\moham\\Desktop\\vuejs-fundamentals-course\\build-a-bot\\node_modules\\file-loader\\dist\\cjs.js',
+              options: {
+                name: 'img/[name].[hash:8].[ext]',
+              },
+            },
+          },
+        },
+      ],
+    };
+    const imagesRuleIndex = config.module.rules.findIndex(x =>
+      x.test.source.includes('png|jpe?g|gif'),
+    );
+    config.module.rules.splice(imagesRuleIndex, 1, newRule);
+  },
   devServer: {
     proxy: {
       '/api': {
